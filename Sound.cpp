e3a1b775e7edc6c0a391ff101aae1ca5c5c274dd
kitamoo <ayaka20111005@yahoo.co.jp>
2025-08-14 08:33:07 +0900

    Soundクラスにメソッド追加

--- Sound.cpp
+++ Sound.cpp
@@ -1,9 +1,9 @@
 #include "Sound.h"
 #include <DxLib.h>
 
+// ゲーム開始時のサウンドロード処理
 void Sound::load()
 {
-	// サウンドの読み込みと音量設定
 	bgm = LoadSoundMem("sound/bgm.mp3");
 	jinOver = LoadSoundMem("sound/gameover.mp3");
 	jinClear = LoadSoundMem("sound/stageclear.mp3");
@@ -14,4 +14,20 @@ void Sound::load()
 	ChangeVolumeSoundMem(128, bgm);
 	ChangeVolumeSoundMem(128, jinOver);
 	ChangeVolumeSoundMem(128, jinClear);
+}
+
+// Bullet発射サウンドのロード処理
+void Sound::loadShotSound(const std::string& filepath)
+{
+	seShotHandle = LoadSoundMem(filepath.c_str());
+	ChangeVolumeSoundMem(128, seShotHandle);
+}
+
+// Bullet発射サウンドの再生
+void Sound::playShot() const
+{
+	if (seShotHandle != -1)
+	{
+		PlaySoundMem(seShotHandle, DX_PLAYTYPE_BACK);
+	}
 }
--- Sound.h
+++ Sound.h
@@ -1,10 +1,17 @@
 #pragma once
+#include <string>
 
 class Sound
 {
 public:
-	Sound() = default;
+	// ゲーム開始時のサウンドロード処理
 	void load();
+
+	// Bullet発射サウンドのロード処理
+	void loadShotSound(const std::string& filePath);
+
+	// Bullet発射サウンドの再生
+	void playShot() const;
 
 	// ゲーム中で使用される音
 	int bgm;
@@ -13,4 +20,7 @@ public:
 	int seExpl;
 	int seItem;
 	int seShot;
+
+private:
+	int seShotHandle = -1;
 };
