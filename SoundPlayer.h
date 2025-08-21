186f6b88b87eac7a39c6aefb42e45083f95da182
kitamoo <ayaka20111005@yahoo.co.jp>
2025-08-21 21:59:10 +0900

    SoundPlayerクラスの修正

--- SoundPlayer.h
+++ SoundPlayer.h
@@ -7,25 +7,17 @@ class SoundPlayer
 public:
 	void play(const Sound& sound)
 	{
-		if (sound.handle != -1)
-		{
 			PlaySoundMem(sound.handle, DX_PLAYTYPE_BACK);
-		}
 	}
 
 	void stop(const Sound& sound)
 	{
-		if (sound.handle != -1)
-		{
 			StopSoundMem(sound.handle);
-		}
 	}
 
 	void setVolume(const Sound& sound, int volume)
 	{
-		if (sound.handle != -1)
-		{
+		
 			ChangeVolumeSoundMem(volume, sound.handle);
-		}
 	}
 };
