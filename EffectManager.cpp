c7fa3ca04adc2fe468cb7c8c1cfd87563b93d5e9
kitamoo <ayaka20111005@yahoo.co.jp>
2025-08-25 14:55:13 +0900

    Effectクラス等の追加

---
+++ EffectManager.cpp
@@ -0,0 +1,47 @@
+#include "EffectManager.h"
+#include "EffectType.h"
+
+void EffectManager::setEffect(int x, int y, int ptn)
+{
+	for (auto& e : effects) {
+		if (e.getState() == 0) {
+			e.setX(x);
+			e.setY(y);
+			e.setVX(0);
+			e.setVY(0);
+			e.setPattern(ptn);
+			e.setImage((ptn == EffectType::Explode) ? GameData::imgExplosion : GameData::imgItem);
+			GetGraphSize(e.getImage(), &e.getWidth(), &e.getHeight());
+			e.setState(1);
+			e.setTimer(0);
+			break;
+		}
+	}
+}
+
+void EffectManager::drawEffect(void)
+{
+	int ix;
+	for (auto& e : effects) {
+		if (e.getState() == 0) continue;
+		switch (e.getPattern()) // エフェクトごとに処理を分ける
+		{
+		case EffectType::Explode: // 爆発演出
+			ix = e.getTimer() * 128; // 画像の切り出し位置
+			DrawRectGraph(e.getX() - 64, e.getY() - 64, ix, 0, 128, 128, e.getImage(), TRUE, FALSE);
+			e.setTimer(e.getTimer() + 1);
+			if (e.getTimer() == 7) e.setState(0);
+			break;
+		case EffectType::Recover: // 回復演出
+			if (e.getTimer() < 30) // 加算による描画の重ね合わせ
+				SetDrawBlendMode(DX_BLENDMODE_ADD, e.getTimer() * 8);
+			else
+				SetDrawBlendMode(DX_BLENDMODE_ADD, (60 - e.getTimer()) * 8);
+			for (int i = 3; i < 8; i++) DrawCircle(e.getX(), e.getY(), (GameData::player.getWidth() + GameData::player.getHeight()) / i, 0x2040c0, TRUE);
+			SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0); // ブレンドモードを解除
+			e.setTimer(e.getTimer() + 1);
+			if (e.getTimer() == 60) e.setState(0);
+			break;
+		}
+	}
+}
---
+++ EffectManager.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Effetct.h"
+
+class EffectManager {
+public:
+	void setEffect(int x, int y, int ptn);
+
+	void drawEffect(void);
+
+private:
+	std::array<Effect, GameConfig::EFFECT_MAX> effects;
+};
