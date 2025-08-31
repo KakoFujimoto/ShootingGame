7797e3fbd9ad9d95103d7872ba09d09170029f1d
kitamoo <ayaka20111005@yahoo.co.jp>
2025-08-31 19:15:14 +0900

    ItemContaineクラスの調整

--- ItemManager.cpp
+++ ItemManager.cpp
@@ -3,7 +3,14 @@
 #include "SoundContainer.h"
 #include "SoundPlayer.h"
 #include "ItemManager.h"
+#include "ItemType.h"
+#include "ImageContainer.h"
+#include "SceneType.h"
+#include "GameManager.h"
+#include "EffectType.h"
 
+ImageContainer img;
+GameManager game;
 
 void ItemManager::setItem(void)
 {
@@ -15,43 +22,86 @@ void ItemManager::setItem(void)
 	item.setTimer(0);
 }
 
-
 // アイテムの処理
-void moveItem(void)
+void ItemManager::moveItem(void)
 {
-	if (GameData::item.state == 0) return;
-	GameData::item.x += GameData::item.vx;
-	GameData::item.y += GameData::item.vy;
-	if (GameData::item.timer % 60 < 30)
-		GameData::item.vx -= 1;
+	if (item.getState() == 0)
+	{
+		return;
+	}
+	item.setX(item.getVX());
+	item.setY(item.getVY());
+
+	if ((item.getTimer() % 60) < 30)
+	{
+		//item.vx -= 1;
+		item.setVX(-1);
+	}
 	else
-		GameData::item.vx += 1;
-	if (GameData::item.y > GameConfig::HEIGHT + 16) GameData::item.state = 0;
-	GameData::item.pattern = (GameData::item.timer / 120) % GameConfig::ITEM_TYPE; // 現在、どのアイテムになっているか
-	GameData::item.timer++;
-	DrawRectGraph(GameData::item.x - 20, GameData::item.y - 16, GameData::item.pattern * 40, 0, 40, 32, GameData::imgItem, TRUE, FALSE);
-	if (GameData::scene == OVER) return; // ゲームオーバー画面では回収できない
-	int dis = (GameData::item.x - GameData::player.x) * (GameData::item.x - GameData::player.x) + (GameData::item.y - GameData::player.y) * (GameData::item.y - GameData::player.y);
+	{
+		//item.vx += 1;
+		item.setVX(1);
+
+	}
+
+	if (item.getY() > GameConfig::HEIGHT + 16)
+	{
+		item.setShield(0);
+	}
+	int index = (item.getTimer() / 120) % GameConfig::ITEM_TYPE; // 現在、どのアイテムになっているか
+	item.setPattern(static_cast<ItemType>(index));
+
+	//item.timer++;
+	item.setTimer(item.getTimer() + 1);
+
+
+	DrawRectGraph(item.getX() - 20,
+		item.getY() - 16,
+		static_cast<int>(item.getPattern()) * 40,
+		0, 40, 32,
+		img.getItem().getId(),
+		TRUE, FALSE);
+
+
+	if (static_cast<int>(SceneType::Over))
+	{
+		return; // ゲームオーバー画面では回収できない
+	}
+
+	int dis = (item.getX() - game.getPlayer().getX()) * (item.getX() - game.getPlayer().getX())
+		+ (item.getY() - game.getPlayer().getY()) * (item.getY() - game.getPlayer().getY());
+
 	if (dis < 60 * 60) // アイテムと自機とのヒットチェック（円による当たり判定）
 	{
-		GameData::item.state = 0;
-		if (GameData::item.pattern == 0) // スピードアップ
+		item.setState(0);
+
+		if(item.getPattern() == ItemType::SppedUp)
 		{
-			if (GameData::player.vx < GameConfig::PLAYER_SPEED_MAX)
+			if (game.getPlayer().getVX() < GameConfig::PLAYER_SPEED_MAX)
 			{
-				GameData::player.vx += 3;
-				GameData::player.vy += 3;
+				//player.vx += 3;
+				game.getPlayer().setVX(3);
+
+				//player.vy += 3;
+				game.getPlayer().setVY(3);
 			}
 		}
-		if (GameData::item.pattern == 1) // シールド回復
+		if (item.getPattern() == ItemType::ShieldRecovery) // シールド回復
 		{
-			if (GameData::player.shield < GameConfig::PLAYER_SHIELD_MAX) GameData::player.shield++;
-			setEffect(GameData::player.x, GameData::player.y, EFF_RECOVER); // 回復エフェクトを表示
+			if (game.getPlayer().getShield() < GameConfig::PLAYER_SHIELD_MAX)
+			{
+				//player.shield++;
+				game.getPlayer().setShield(game.getPlayer().getShield() + 1);
+
+			}
+
+			game.getEffect().setEffect(game.getPlayer().getX(), game.getPlayer().getY(), EffectType::Recover, game.getImage()); // 回復エフェクトを表示
 		}
-		if (GameData::item.pattern == 2) // 武器レベルアップ
+		if (item.getPattern() == ItemType::WeaponLvUp) // 武器レベルアップ
 		{
 			if (GameData::weaponLv < GameConfig::WEAPON_LV_MAX) GameData::weaponLv++;
 		}
-		PlaySoundMem(GameData::seItem, DX_PLAYTYPE_BACK); // 効果音
+		//PlaySoundMem(seItem, DX_PLAYTYPE_BACK); // 効果音
+		game.getSoundPlayer().play(game.getSoundContainer().seItem);
 	}
 }
