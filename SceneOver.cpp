65360bf5a39b559f61a5278a866cc57070055e98
kitamoo <ayaka20111005@yahoo.co.jp>
2025-09-02 12:29:57 +0900

    SceneOverまわりの調整

---
+++ SceneOver.cpp
@@ -0,0 +1,33 @@
+#include "SceneOver.h"
+#include "SceneType.h"
+
+std::shared_ptr<IScene> SceneOver::run(GameManager& game)
+{
+		if (game.getGameData().timer < GameConfig::FPS * 3) // Ž©‹@‚ª”š”­‚·‚é‰‰o
+
+	{
+			if (game.getGameData().timer % 7 == 0)
+			{
+				game.getEffect().setEffect
+				(
+					game.getPlayer().getX() + rand() % 81 - 40,
+					game.getPlayer().getY() + rand() % 81 - 40,
+					EffectType::Explode,
+					game.getImage()
+				);
+			}
+	}
+	else if (game.getGameData().timer == GameConfig::FPS * 3)
+	{
+		game.getSoundPlayer().play(game.getSoundContainer().jinOver);
+	}
+	else
+	{
+			game.getDrawer().drawTextC(GameConfig::WIDTH * 0.5, GameConfig::HEIGHT * 0.3, "GAME OVER", 0xff0000, 80);
+	}
+		if (game.getGameData().timer > GameConfig::FPS * 10)
+		{
+			return std::make_shared<SceneTitle>();
+		}
+	return nullptr;
+}
--- SceneOver.h
+++ SceneOver.h
@@ -8,21 +8,5 @@ class SceneOver
 	: IScene
 {
 public:
-	virtual std::shared_ptr<IScene> run(GameManager& game)
-	{
-			if (GameData::timer < GameConfig::FPS * 3) // 自機が爆発する演出
-			{
-				if (GameData::timer % 7 == 0) setEffect(GameData::player.x + rand() % 81 - 40, GameData::player.y + rand() % 81 - 40, EFF_EXPLODE);
-			}
-			else if (GameData::timer == GameConfig::FPS * 3)
-			{
-				PlaySoundMem(GameData::jinOver, DX_PLAYTYPE_BACK); // ジングル出力
-			}
-			else
-			{
-				drawTextC(GameConfig::WIDTH * 0.5, GameConfig::HEIGHT * 0.3, "GAME OVER", 0xff0000, 80);
-			}
-			if (GameData::timer > GameConfig::FPS * 10) GameData::scene = TITLE; // タイトルへ遷移
-			break;
-	}
+	virtual std::shared_ptr<IScene> run(GameManager& game);
 };
